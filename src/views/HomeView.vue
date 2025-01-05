462e81fa908e344524c36990083b0b95b20d0ff0
ingenious-dev <ingeniousdevke@gmail.com>
2025-01-05 20:09:35 +0300

    Provide demo video

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,7 +35,7 @@
               <div class="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 <div class="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                   <RouterLink :to="{name: 'activities'}" class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8">Get started</RouterLink>
-                  <!-- <RouterLink href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8">Live demo</RouterLink> -->
+                  <a href="https://youtu.be/GrZqqnNRzZU" target="_blank" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8">Live demo</a>
                 </div>
               </div>
             </div>
@@ -164,10 +164,10 @@
         <div class="mx-auto max-w-4xl py-16 px-6 sm:py-24 lg:flex lg:max-w-7xl lg:items-center lg:justify-between lg:px-8">
           <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             <span class="block">Ready to get started?</span>
-            <span class="-mb-1 block bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text pb-1 text-transparent">Get in touch or create an account.</span>
+            <span class="-mb-1 block bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text pb-1 text-transparent">Watch demo or create an account.</span>
           </h2>
           <div class="mt-6 space-y-4 sm:flex sm:space-y-0 sm:space-x-5">
-            <!-- <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700">Learn more</a> -->
+            <a href="https://youtu.be/GrZqqnNRzZU" target="_blank" class="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700">Watch demo</a>
             <RouterLink :to="{name: 'activities'}" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-50 px-4 py-3 text-base font-medium text-indigo-800 shadow-sm hover:bg-indigo-100">Get started</RouterLink>
           </div>
         </div>

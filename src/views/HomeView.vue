7a48fff5803e9dc4d7609903527ac938c3918231
ingenious-dev <ingeniousdevke@gmail.com>
2025-01-05 18:47:29 +0300

    Update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -110,7 +110,7 @@
             </div>
             <div class="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
               <div class="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
-                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="static/remana/Screenshot 2025-01-05 174825.png" alt="Customer profile user interface" />
+                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="/static/remana/Screenshot 2025-01-05 174825.png" alt="Customer profile user interface" />
               </div>
             </div>
           </div>
